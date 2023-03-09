5290b21f181f5e3c5f068fc139bd9ef442b9d796
Ali <alipaz1381@gmail.com>
2023-03-09 16:52:11 +0330

    feat: add new fillable for user table create const of columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,21 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    const TABLE_NAME = 'users';
+
+    const COLUMN_ID                    = 'id';
+    const COLUMN_FIRST_NAME            = 'first_name';
+    const COLUMN_LAST_NAME             = 'last_name';
+    const COLUMN_EMAIL                 = 'email';
+    const COLUMN_EMAIL_VERIFIED_AT     = 'email_verified_at';
+    const COLUMN_PHONE_NUMBER          = 'phone_number';
+    const COLUMN_MOBILE_VERIFIED_AT    = 'mobile_verified_at';
+    const COLUMN_CARD_NUMBER           = 'card_number';
+    const COLUMN_PASSWORD              = 'password';
+    const COLUMN_REMEMBER_TOKEN        = 'remember_token';
+
+
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -18,9 +33,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        self::COLUMN_FIRST_NAME,
+        self::COLUMN_LAST_NAME,
+        self::COLUMN_EMAIL,
+        self::COLUMN_PASSWORD,
     ];
 
     /**
