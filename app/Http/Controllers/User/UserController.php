e0c15b7e37cace110947724e0bb52abf7b3501ef
Ali <alipaz1381@gmail.com>
2023-03-09 16:55:58 +0330

    feat: add new function for store new user store function in UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\User;
 
+use App\DataTransferObject\UsersDto;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\User\StoreUserValidation;
+use App\Services\User\UserService;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
+
+
+    public function __construct(
+        protected UserService $userService,
+    )
+    {
+    }
+
+    /**
+     * @param StoreUserValidation $userValidationRequest
+     * @return void
+     */
+    public function store(StoreUserValidation $userValidationRequest)
+    {
+      return $this->userService->registerNewUser(
+            UsersDto::fromUserRequest($userValidationRequest)
+        );
+
+    }
 
 }
