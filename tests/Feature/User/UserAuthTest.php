c196d9edcf533a8f395eb2b4348854c62d2842ea
Ali <alipaz1381@gmail.com>
2023-03-09 16:41:23 +0330

    test: make user can log in test check status code and database has

--- tests/Feature/User/UserAuthTest.php
+++ tests/Feature/User/UserAuthTest.php
@@ -2,22 +2,35 @@
 
 namespace Tests\Feature\User;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class UserAuthTest extends TestCase
 {
-    use
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_register()
     {
-        $response = $this->get('/');
+        $data = [
+            User::COLUMN_FIRST_NAME => 'John',
+            User::COLUMN_LAST_NAME => 'Doe',
+            User::COLUMN_EMAIL => 'john.doe@example.com',
+            User::COLUMN_PASSWORD => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post(route('user.register'), $data);
 
         $response->assertStatus(200);
+
+        $this->assertDatabaseHas('users', [
+            User::COLUMN_FIRST_NAME => 'John',
+            User::COLUMN_LAST_NAME => 'Doe',
+            User::COLUMN_EMAIL => 'john.doe@example.com',
+        ]);
     }
 }
