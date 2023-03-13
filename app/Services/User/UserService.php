09a82775af21b97474acce9034f62e7a49c516d3
Ali <alipaz1381@gmail.com>
2023-03-13 15:49:01 +0330

    feat: user service add login and log out function

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -4,23 +4,44 @@ namespace App\Services\User;
 
 use App\DataTransferObject\UsersDto;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class UserService
 {
+
+
     /**
      * @param UsersDto $usersDto
      * @return mixed
      */
-    public function registerNewUser(UsersDto $usersDto)
+    public function registerNewUser(UsersDto $usersDto): mixed
     {
         return DB::transaction(function () use ($usersDto) {
-            User::create([
+            $user = User::create([
                 User::COLUMN_FIRST_NAME => $usersDto->firstName,
                 User::COLUMN_LAST_NAME  => $usersDto->lastName,
                 User::COLUMN_EMAIL      => $usersDto->email,
-                User::COLUMN_PASSWORD   => $usersDto->password
+                User::COLUMN_PASSWORD   => bcrypt($usersDto->password)
             ]);
+
+            return $user;
         });
+    }
+
+
+    public function login(array $data)
+    {
+        $credentials = ['email' => $data['email'], 'password' => $data['password']];
+
+        $remember = isset($data['remember']) && $data['remember'] === 'on';
+
+        return Auth::attempt($credentials, $remember);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
     }
 }
