03e1fe0d501ffda4cc29195f362fc8cbdcf51b3f
Ali <alipaz1381@gmail.com>
2023-03-13 10:12:48 +0330

    feat: add new Controller for checkout

---
+++ app/Http/Controllers/Checkout/CheckoutController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Checkout;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class CheckoutController extends Controller
+{
+
+    /**
+     * @param Order $order
+     * @return View
+     */
+    public function show(Order $order): View
+    {
+        return view('checkout.show', compact('order'));
+    }
+
+    public function pay()
+    {
+
+    }
+}
