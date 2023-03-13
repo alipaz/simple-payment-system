d76ecef8ec1bad1149b32d820cad3656b6314b99
Ali <alipaz1381@gmail.com>
2023-03-13 15:50:24 +0330

    feat: add new login logout function in user controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\User;
 
+use App\DataTransferObject\UserLoginDto;
 use App\DataTransferObject\UsersDto;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreUserValidation;
+use App\Http\Requests\User\UserLoginRequest;
 use App\Services\User\UserService;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -24,9 +27,32 @@ class UserController extends Controller
      */
     public function store(StoreUserValidation $userValidationRequest)
     {
-      return $this->userService->registerNewUser(
+        $user =   $this->userService->registerNewUser(
             UsersDto::fromUserStoreRequest($userValidationRequest)
         );
+
+        Auth::login($user);
+
+        return redirect()->route('checkout.show', ['order' => 1]);
+    }
+
+    public function login(UserLoginRequest $loginRequest)
+    {
+        $success = $this->userService->login(
+            UserLoginDto::fromUserLoginRequest($loginRequest)
+        );
+
+        if ($success) {
+            return redirect()->route('checkout.show', ['order' => 1]);
+        } else {
+            return redirect()->back()->withErrors(['email' => 'Invalid email or password.']);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('user.create');
     }
 
     /**
